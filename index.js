77f431f48643866504594ac0c442beeb9ce4eff8
RodrigoJambeiro <rodrigo.portela@ucsal.edu.br>
2021-04-27 08:29:29 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+import './css/bootstrap.min.css'
+import './js/bootstrap.bundle.min.js';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
